90f141a6c72df56df048e52ffe2404839b10fda6
Katharina Löffler <katharina.loeffler.1501@gmail.com>
2022-05-08 16:00:39 +0300

    add egocentric alignment utils for themis projects

--- themis_pipeline.py
+++ themis_pipeline.py
@@ -1,60 +1,67 @@
-from vame.util.prep_themis_data import get_video_metadata, pickle_dlc_to_df
 import pandas as pd
 import re
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn
-
+import vame
+from vame.initialize_project.themis_new import init_new_project
+from vame.util.align_egocentrical_themis import egocentric_alignment
 
 VIDEO_ROOT = "/media/Themis/Data/Video"
-
-# in this dir there is currently only one pkl file
 PKL_ROOT = "/media/Themis/Data/Models/3843S2B10Gaussians/analyses"
-VIS_RES_PATH = "/home/katharina/vame_approach/results/landmark_conf_distribution"
 
-# where to save the create landmark.csv files that include landmark positions and their confidence
-SAVE_DF_PATH = "/home/katharina/vame_approach/landmark_data"
-if not os.path.exists(SAVE_DF_PATH):
-    os.makedirs(SAVE_DF_PATH)
+PROJECT_PATH = "/home/katharina/vame_approach/themis_dummy"
 
-pkl_files = [
-    os.path.join(PKL_ROOT, element)
-    for element in os.listdir(PKL_ROOT)
-    if element.endswith(".pkl")
-]
+CREATE_NEW_PROJECT = False
+TRAIN_MODEL = True
+EVAL_MODEL = False
+VISUALIZE_MODEL = False
 
-video_df = get_video_metadata(VIDEO_ROOT, DLC_model="")
-video_df["video_id"] = video_df["vid_file"].apply(lambda x: x.split(".")[0])
-# map videos to pkl files
-video_ids = [re.findall(r"\d+", pkl_file)[0] for pkl_file in pkl_files]
-video_df = video_df[np.isin(video_df["video_id"].values, video_ids)]
+# create landmark.csv files including the landmark positions and likelihood (confidence scores)
+# similar to the DLC files and do some simple visualization of the confidence scores
 
-# pkl to dataframe
-for pkl_file in pkl_files:
-    dlc_file = os.path.join(
-        PKL_ROOT, "_".join(os.path.basename(pkl_file).split("_")[:-1]) + ".csv"
-    )
-    if not os.path.exists(dlc_file):
-        print(f"No file named {dlc_file}")
-        continue
-    landmark_names = pd.read_csv(dlc_file, header=[0, 1]).columns
-    df, df_conf = pickle_dlc_to_df(pkl_file, landmark_names, return_conf_df=True)
-    # drop the likelihood part
-    col_names = {col_name: col_name[0] for col_name in df_conf.columns}
-    df_conf.rename(columns=col_names, inplace=True)
 
-    print("--" * 10)
-    print(f"Landmark Confidence Quantiles of {pkl_file}")
-    print(df_conf.quantile([0.1, 0.5, 0.9]))
-    print("--" * 10)
+# initialize new project
+if CREATE_NEW_PROJECT:
+    config = init_new_project(PROJECT_PATH, VIDEO_ROOT, PKL_ROOT)
+else:
+    config = os.path.join(PROJECT_PATH, "config.yaml")
 
-    plot = seaborn.violinplot(data=df_conf[::100], orient="h")
-    plot.set(title="Distribution of Confidence Scores")
-    plt.tight_layout()
-    plot.get_figure().savefig(
-        os.path.join(VIS_RES_PATH, os.path.basename(pkl_file).split(".")[0] + ".png")
-    )
+if TRAIN_MODEL:
+    # TODO: add some checks concerning the threshold - otherwise many datapoints will be rejected
+    # cross check with example.csv how many datapoints are rejected
 
-    # save file to disk
-    df.to_csv(os.path.join(SAVE_DF_PATH))
+    egocentric_alignment(PROJECT_PATH, pose_ref_index=[0, 5])
+    print()
+    # 2.3 training data generation
+    vame.create_trainset(config)
+
+    # 3 VAME training
+    vame.train_model(config)
+if EVAL_MODEL:
+    # 4 Evaluate trained model
+    vame.evaluate_model(config)
+if VISUALIZE_MODEL:
+    # 5 segment motifs/pose; output latent_vector..npy file
+    vame.pose_segmentation(config)
+
+    # there are many options to for visualization
+    # OPTIONIAL: Create motif videos to get insights about the fine grained poses
+    vame.motif_videos(config, videoType=".mp4")
+
+    # OPTIONAL: Create behavioural hierarchies via community detection
+    vame.community(config, show_umap=True, cut_tree=2)
+
+    # OPTIONAL: Create community videos to get insights about behavior on a hierarchical scale
+    vame.community_videos(config)
+
+    # OPTIONAL: Down projection of latent vectors and visualization via UMAP
+    vame.visualization(config, label=None)  # options: label: None, "motif", "community"
+
+    # OPTIONAL: Use the generative model (reconstruction decoder) to sample from
+    # the learned data distribution, reconstruct random real samples or visualize
+    # the cluster center for validation
+    vame.generative_model(
+        config, mode="centers"
+    )  # options: mode: "sampling", "reconstruction", "centers", "motifs"
---
+++ vame/util/align_egocentrical_themis.py
@@ -0,0 +1,300 @@
+"""
+Adapt align_egocentrial such that it works with the adapted file structure
+(videos won't be copied but are kept as .csv file with paths to them and
+instead of evaluation by videos the evaluation is done by the landmark predictions
+since different models where trained that need to be compared)
+"""
+import os
+import cv2 as cv
+import numpy as np
+import pandas as pd
+import tqdm
+
+from pathlib import Path
+from vame.util.auxiliary import read_config
+from vame.util.align_egocentrical import interpol, crop_and_flip, play_aligned_video
+import re
+
+
+def align_mouse(
+    project_dir,
+    landmark_file_name,
+    crop_size,
+    pose_list,
+    pose_ref_index,
+    confidence,
+    pose_flip_ref,
+    bg,
+    frame_count,
+    use_video=True,
+):
+    # returns: list of cropped images (if video is used) and list of cropped DLC points
+    #
+    # parameters:
+    # project_dir: project directory
+    # landmark_file_name: name of the landmark file to process
+    # crop_size: tuple of x and y crop size
+    # pose_list: list of arrays containg corresponding x and y DLC values
+    # pose_ref_index: indices of 2 lists in dlc_list to align mouse along
+    # pose_flip_ref: indices of 2 lists in dlc_list to flip mouse if flip was false
+    # bg: background image to subtract
+    # frame_count: number of frames to align
+    # use_video: boolean if video should be cropped or DLC points only
+
+    images = []
+    points = []
+
+    # get path to the corresponding video file
+    video_df = pd.read_csv(os.path.join(project_dir, "video_info.csv"))
+    video_id = int(re.findall(r"\d+", landmark_file_name)[0])
+    video_file = os.path.join(
+        *video_df[video_df["video_id"] == video_id][["vid_folder", "vid_file"]].values[
+            0
+        ]
+    )
+
+    for i in pose_list:
+        for j in i:
+            if j[2] <= confidence:
+                j[0], j[1] = np.nan, np.nan
+
+    for i in pose_list:
+        i = interpol(i)
+
+    if use_video:
+        capture = cv.VideoCapture(os.path.join(video_file))
+
+        if not capture.isOpened():
+            raise Exception(
+                "Unable to open video file: {0}".format(os.path.join(video_file))
+            )
+
+    for idx in tqdm.tqdm(range(frame_count), disable=not True, desc="Align frames"):
+
+        if use_video:
+            # Read frame
+            try:
+                ret, frame = capture.read()
+                frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+                frame = frame - bg
+                frame[frame <= 0] = 0
+            except:
+                print("Couldn't find a frame in capture.read(). #Frame: %d" % idx)
+                continue
+        else:
+            frame = np.zeros((1, 1))
+
+        # Read coordinates and add border
+        pose_list_bordered = []
+
+        for i in pose_list:
+            pose_list_bordered.append(
+                (int(i[idx][0] + crop_size[0]), int(i[idx][1] + crop_size[1]))
+            )
+
+        img = cv.copyMakeBorder(
+            frame,
+            crop_size[1],
+            crop_size[1],
+            crop_size[0],
+            crop_size[0],
+            cv.BORDER_CONSTANT,
+            0,
+        )
+
+        punkte = []
+        for i in pose_ref_index:
+            coord = []
+            coord.append(pose_list_bordered[i][0])
+            coord.append(pose_list_bordered[i][1])
+            punkte.append(coord)
+        punkte = [punkte]
+        punkte = np.asarray(punkte)
+
+        # calculate minimal rectangle around snout and tail
+        rect = cv.minAreaRect(punkte)
+
+        # change size in rect tuple structure to be equal to crop_size
+        lst = list(rect)
+        lst[1] = crop_size
+        rect = tuple(lst)
+
+        center, size, theta = rect
+
+        # crop image
+        out, shifted_points = crop_and_flip(
+            rect, img, pose_list_bordered, pose_flip_ref
+        )
+
+        if use_video:  # for memory optimization, just save images when video is used.
+            images.append(out)
+        points.append(shifted_points)
+
+    if use_video:
+        capture.release()
+
+    time_series = np.zeros((len(pose_list) * 2, frame_count))
+    for i in range(frame_count):
+        idx = 0
+        for j in range(len(pose_list)):
+            time_series[idx : idx + 2, i] = points[i][j]
+            idx += 2
+
+    return images, points, time_series
+
+
+def egocentric_alignment(
+    project_path,
+    pose_ref_index=[0, 5],
+    crop_size=(300, 300),
+    use_video=False,
+    check_video=False,
+):
+    """ Happy aligning """
+    config = os.path.join(project_path, "config.yaml")
+    video_df = pd.read_csv(os.path.join(project_path, "video_info.csv"))
+
+    # config parameters
+    config_file = Path(config).resolve()
+    cfg = read_config(config_file)
+
+    path_to_file = cfg["project_path"]
+    landmark_file_names = cfg["video_sets"]
+    confidence = cfg["pose_confidence"]
+    crop_size = crop_size
+
+    # call function and save into your VAME data folder
+    for lm_file_name in landmark_file_names:
+        print(
+            "Aligning data %s, Pose confidence value: %.2f" % (lm_file_name, confidence)
+        )
+        video_id = int(re.findall(r"\d+", lm_file_name)[0])
+        video_file = os.path.join(
+            *video_df[video_df["video_id"] == video_id][
+                ["vid_folder", "vid_file"]
+            ].values[0]
+        )
+        egocentric_time_series, frames = alignment(
+            video_file,
+            project_path,
+            lm_file_name,
+            pose_ref_index,
+            crop_size,
+            confidence,
+            use_video=use_video,
+            check_video=check_video,
+        )
+        np.save(
+            os.path.join(
+                path_to_file, "data", lm_file_name, lm_file_name + "-PE-seq.npy"
+            ),
+            egocentric_time_series,
+        )
+    #        np.save(os.path.join(path_to_file,'data/',file,"",file+'-PE-seq.npy', egocentric_time_series))
+
+    print("Your data is now ine right format and you can call vame.create_trainset()")
+
+
+def alignment(
+    video_file,
+    project_dir,
+    landmark_file_name,
+    pose_ref_index,
+    crop_size,
+    confidence,
+    use_video=False,
+    check_video=False,
+):
+
+    # read out data
+    data = pd.read_csv(
+        os.path.join(project_dir, "landmarks", landmark_file_name + ".csv"), skiprows=2,
+    )
+    data_mat = pd.DataFrame.to_numpy(data)
+    data_mat = data_mat[:, 1:]
+
+    # get the coordinates for alignment from data table
+    pose_list = []
+
+    for i in range(int(data_mat.shape[1] / 3)):
+        pose_list.append(data_mat[:, i * 3 : (i + 1) * 3])
+
+    # list of reference coordinate indices for alignment
+    # 0: snout, 1: forehand_left, 2: forehand_right,
+    # 3: hindleft, 4: hindright, 5: tail
+
+    pose_ref_index = pose_ref_index
+
+    # list of 2 reference coordinate indices for avoiding flipping
+    pose_flip_ref = pose_ref_index
+
+    if use_video:
+        # compute background
+        bg = background(video_file)
+        capture = cv.VideoCapture(video_file)
+        if not capture.isOpened():
+            raise Exception("Unable to open video file: {0}".format(video_file))
+
+        frame_count = int(capture.get(cv.CAP_PROP_FRAME_COUNT))
+        capture.release()
+    else:
+        bg = 0
+        frame_count = len(
+            data
+        )  # Change this to an abitrary number if you first want to test the code
+
+    frames, n, time_series = align_mouse(
+        project_dir,
+        landmark_file_name,
+        crop_size,
+        pose_list,
+        pose_ref_index,
+        confidence,
+        pose_flip_ref,
+        bg,
+        frame_count,
+        use_video,
+    )
+
+    if check_video:
+        play_aligned_video(frames, n, frame_count)
+
+    return time_series, frames
+
+
+def background(video_file, num_frames=1000):
+    """
+    Compute background image from fixed camera 
+    """
+    import scipy.ndimage
+
+    capture = cv.VideoCapture(video_file)
+
+    if not capture.isOpened():
+        raise Exception("Unable to open video file: {0}".format(video_file))
+
+    frame_count = int(capture.get(cv.CAP_PROP_FRAME_COUNT))
+    ret, frame = capture.read()
+
+    height, width, _ = frame.shape
+    frames = np.zeros((height, width, num_frames))
+
+    for i in tqdm.tqdm(
+        range(num_frames),
+        disable=not True,
+        desc="Compute background image for video %s" % video_file,
+    ):
+        rand = np.random.choice(frame_count, replace=False)
+        capture.set(1, rand)
+        ret, frame = capture.read()
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        frames[..., i] = gray
+
+    print("Finishing up!")
+    medFrame = np.median(frames, 2)
+    background = scipy.ndimage.median_filter(medFrame, (5, 5))
+
+    # np.save(path_to_file+'videos/'+'background/'+filename+'-background.npy',background)
+
+    capture.release()
+    return background
