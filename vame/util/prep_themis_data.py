b628af7b366af2b7b23f3fa4c1a560515772e5fc
Katharina Löffler <katharina.loeffler.1501@gmail.com>
2022-05-08 15:59:34 +0300

    fix intendation

--- vame/util/prep_themis_data.py
+++ vame/util/prep_themis_data.py
@@ -64,35 +64,31 @@ def pickle_dlc_to_df(pkl_file, df_header, return_conf_df=False):
     """Convert pkl file with landmarks and confidence measures to a dataframe similar as in DLC."""
     with open(pkl_file, "rb") as file:
         data = pickle.load(file)
-        landmark_positions = data["orig_lm"]
-        landmark_confidence = data["confidence"]["conf"]
-        # create nested header landmark_name: x,y, likelihood
-        lm_index, landmark_names = df_header[0], df_header[1:]
+    landmark_positions = data["orig_lm"]
+    landmark_confidence = data["confidence"]["conf"]
+    # create nested header landmark_name: x,y, likelihood
+    lm_index, landmark_names = df_header[0], df_header[1:]
 
-        landmark_position_df = pd.DataFrame(
-            landmark_positions.reshape(landmark_positions.shape[0], -1),
-            columns=landmark_names,
-        )
-        # keep the same order of the landmarks and replace dim with confidence
-        # - one confidence score per landmark (x,y pair)
-        landmark_conf_names = [
-            (lm[0], "likelihood") for lm in landmark_names if lm[-1] == "x"
-        ]
-        landmark_conf_df = pd.DataFrame(
-            landmark_confidence, columns=landmark_conf_names
-        )
+    landmark_position_df = pd.DataFrame(
+        landmark_positions.reshape(landmark_positions.shape[0], -1),
+        columns=landmark_names,
+    )
+    # keep the same order of the landmarks and replace dim with confidence
+    # - one confidence score per landmark (x,y pair)
+    landmark_conf_names = [
+        (lm[0], "likelihood") for lm in landmark_names if lm[-1] == "x"
+    ]
+    landmark_conf_df = pd.DataFrame(landmark_confidence, columns=landmark_conf_names)
 
-        landmark_df = pd.concat([landmark_position_df, landmark_conf_df], axis=1)
+    landmark_df = pd.concat([landmark_position_df, landmark_conf_df], axis=1)
 
-        # rearrange df such that bodypart1: x,y, likelihood; bodypart2:x,y,likelihood
-        columns_sorted = [
-            (lm[0], dim)
-            for lm in landmark_conf_names
-            for dim in ["x", "y", "likelihood"]
-        ]
+    # rearrange df such that bodypart1: x,y, likelihood; bodypart2:x,y,likelihood
+    columns_sorted = [
+        (lm[0], dim) for lm in landmark_conf_names for dim in ["x", "y", "likelihood"]
+    ]
 
-        landmark_df = landmark_df[columns_sorted]
-        landmark_df.index.name = lm_index
-        if return_conf_df:
-            return landmark_df, landmark_conf_df
-        return landmark_df
+    landmark_df = landmark_df[columns_sorted]
+    landmark_df.index.name = lm_index
+    if return_conf_df:
+        return landmark_df, landmark_conf_df
+    return landmark_df
