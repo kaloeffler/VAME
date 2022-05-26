cd95785a24298b30e332198566c4b359e25e2947
Katharina Löffler <katharina.loeffler.1501@gmail.com>
2022-05-26 15:08:22 +0300

    edit script to create aligned mouse videos

--- analysis_scripts/create_aligned_videos.py
+++ analysis_scripts/create_aligned_videos.py
@@ -3,9 +3,15 @@ import sys
 sys.path.insert(0, "/home/katharina/vame_approach/VAME")
 import os
 import numpy as np
-from vame.analysis.visualize import create_aligned_mouse_video, create_pose_snipplet
+from vame.analysis.visualize import (
+    create_aligned_mouse_video,
+    create_pose_snipplet,
+    thin_dataset_iteratively,
+)
 import pandas as pd
 import cv2 as cv
+from datetime import datetime
+from vame.util.auxiliary import read_config
 
 PROJECT_PATH = "/home/katharina/vame_approach/themis_tail_belly_align"
 video_file = "/media/Themis/Data/Video/HO1/2021-01-11/Down/0057.MP4"
@@ -14,6 +20,18 @@ align_landmark_idx = (8, 16)
 save_aligned_video_path = (
     "/home/katharina/vame_approach/themis_tail_belly_align/results/align"
 )
+
+trained_models = [
+    (datetime.strptime(element, "%m-%d-%Y-%H-%M"), element)
+    for element in os.listdir(os.path.join(PROJECT_PATH, "model"))
+]
+# sort by time step
+trained_models.sort(key=lambda x: x[0])
+latest_model = trained_models[-1][-1]
+
+config_file = os.path.join(PROJECT_PATH, "model", latest_model, "config.yaml")
+config = read_config(config_file)
+
 
 if not os.path.exists(save_aligned_video_path):
     os.makedirs(save_aligned_video_path)
@@ -34,7 +52,9 @@ if not os.path.exists(pose_video_file):
     landmark_name = os.path.basename(landmark_file).split(".")[0]
     # reshape to (N_samples, N_landmarks, 2)
     landmark_data_aligned = np.load(
-        os.path.join(PROJECT_PATH, "data", landmark_name, landmark_name + "-PE-seq.npy")
+        os.path.join(
+            PROJECT_PATH, "data", landmark_name, landmark_name + "-PE-seq.npy"
+        )
     ).T
     landmark_data_aligned = landmark_data_aligned.reshape(
         landmark_data_aligned.shape[0], -1, 2
@@ -47,7 +67,9 @@ if not os.path.exists(pose_video_file):
     capture = cv.VideoCapture(os.path.join(video_file))
 
     column_names = pd.read_csv(landmark_file, header=[0, 1]).columns
-    landmark_names = [col_name[0] for col_name in column_names if col_name[-1] == "x"]
+    landmark_names = [
+        col_name[0] for col_name in column_names if col_name[-1] == "x"
+    ]
     time_ids = np.arange(0, len(landmark_data_trafo))
     create_pose_snipplet(
         landmark_data_trafo,
