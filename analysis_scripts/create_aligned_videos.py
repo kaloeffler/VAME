dd407de1b2f30821dff541655cb9e9db045c7931
Katharina Löffler <katharina.loeffler.1501@gmail.com>
2022-06-08 14:38:30 +0300

    add flag to create landmark pose video only if demanded

--- analysis_scripts/create_aligned_videos.py
+++ analysis_scripts/create_aligned_videos.py
@@ -18,6 +18,11 @@ PROJECT_PATH = "/home/katharina/vame_approach/tb_align_0044_0089"
 VIDEO_ROOT = "/media/Themis/Data/Video"
 
 save_aligned_video_path = os.path.join(PROJECT_PATH, "videos/aligned_videos")
+
+
+# create a video showing the aligned landmarks only in addition to the cropped video
+CREATE_POSE_VIDEO = False
+
 for lm_file in os.listdir(os.path.join(PROJECT_PATH, "landmarks")):
     landmark_file = os.path.join(PROJECT_PATH, "landmarks", lm_file)
 
@@ -47,41 +52,44 @@ for lm_file in os.listdir(os.path.join(PROJECT_PATH, "landmarks")):
     )
 
     video_name = os.path.basename(video_file)
-    pose_video_file = os.path.join(PROJECT_PATH, "videos", "poses", "p" + video_name)
-    if not os.path.exists(pose_video_file):
-        crop_size = 400
-        # min max normalize the data to a fixed grid shape for visualization
-        landmark_name = os.path.basename(landmark_file).split(".")[0]
-        # reshape to (N_samples, N_landmarks, 2)
-        landmark_data_aligned = np.load(
-            os.path.join(
-                PROJECT_PATH,
-                "inference",
-                "data",
-                landmark_name,
-                landmark_name + "-PE-seq.npy",
+    if CREATE_POSE_VIDEO:
+        pose_video_file = os.path.join(
+            PROJECT_PATH, "videos", "poses", "p" + video_name
+        )
+        if not os.path.exists(pose_video_file):
+            crop_size = 400
+            # min max normalize the data to a fixed grid shape for visualization
+            landmark_name = os.path.basename(landmark_file).split(".")[0]
+            # reshape to (N_samples, N_landmarks, 2)
+            landmark_data_aligned = np.load(
+                os.path.join(
+                    PROJECT_PATH,
+                    "inference",
+                    "data",
+                    landmark_name,
+                    landmark_name + "-PE-seq.npy",
+                )
+            ).T
+            landmark_data_aligned = landmark_data_aligned.reshape(
+                landmark_data_aligned.shape[0], -1, 2
             )
-        ).T
-        landmark_data_aligned = landmark_data_aligned.reshape(
-            landmark_data_aligned.shape[0], -1, 2
-        )
-        landmark_data_trafo = (
-            (landmark_data_aligned - landmark_data_aligned.min())
-            / (landmark_data_aligned.max() - landmark_data_aligned.min())
-            * (crop_size - 1)
-        )
-        capture = cv.VideoCapture(os.path.join(video_file))
+            landmark_data_trafo = (
+                (landmark_data_aligned - landmark_data_aligned.min())
+                / (landmark_data_aligned.max() - landmark_data_aligned.min())
+                * (crop_size - 1)
+            )
+            capture = cv.VideoCapture(os.path.join(video_file))
 
-        column_names = pd.read_csv(landmark_file, header=[0, 1]).columns
-        landmark_names = [
-            col_name[0] for col_name in column_names if col_name[-1] == "x"
-        ]
-        time_ids = np.arange(0, len(landmark_data_trafo))
-        create_pose_snipplet(
-            landmark_data_trafo,
-            landmark_names,
-            time_ids,
-            pose_video_file,
-            crop_size=(crop_size, crop_size),
-            fps=capture.get(cv.CAP_PROP_FPS),
-        )
+            column_names = pd.read_csv(landmark_file, header=[0, 1]).columns
+            landmark_names = [
+                col_name[0] for col_name in column_names if col_name[-1] == "x"
+            ]
+            time_ids = np.arange(0, len(landmark_data_trafo))
+            create_pose_snipplet(
+                landmark_data_trafo,
+                landmark_names,
+                time_ids,
+                pose_video_file,
+                crop_size=(crop_size, crop_size),
+                fps=capture.get(cv.CAP_PROP_FPS),
+            )
